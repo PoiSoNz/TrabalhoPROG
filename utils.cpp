a541feb2c82f208740b56d8b8ed9013211a7e7b3
carbap <aca.baptista@gmail.com>
2016-05-11 01:19:49 +0100

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -38,32 +38,3 @@ void checkFile(ifstream &stream, string nomeFicheiro)//verifica se um ficheiro f
 	cout << "O ficheiro foi lido com sucesso: ";
 }
 
-Cliente fillClientsVector(ifstream &stream)//
-{
-	string line;
-	int lineCount = 0;
-	while (getline(stream, line))
-	{
-		if (!lineCount == 0)
-		{
-			int index1 = line.find_first_of(';');
-			int index2 = (line.substr(index1)).find_first_of(';');
-			int index3 = line.find_last_of(';');
-			string id = line.substr(0, index1 - 1);
-			string name = line.substr(index1 + 2, (index2 - 1) - (index1 + 2));
-			Data Data(line.substr(index2 + 2, (index3 - 1) - (index2 + 2)));
-			string value = line.substr(index3 + 2);
-			
-			(esta parte é copy paste do t1 que era feito com structs)
-			Cliente c;
-			c.id = stoi(id);
-			c.name = name;
-			c.amount = stod(value);
-			
-			
-			//VendeMaisMais::clientes.push_back(c);
-			lineCount++;
-		}
-	}
-}
-
