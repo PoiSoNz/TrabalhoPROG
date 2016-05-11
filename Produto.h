032e03633c07b6e71c95b010539cbc5aa14a19ac
carbap <aca.baptista@gmail.com>
2016-05-11 19:50:39 +0100

    Update Produto.h

--- Produto.h
+++ Produto.h
@@ -19,6 +19,7 @@ class Produto {
 
  public:
   Produto(ifstream & in);
+  Produto(string nome, float custo);
   string getNome() const;
   float getCusto() const;
   int indiceProduto(string nome); // uso do map para "traduzir" o nome em indice no vetor de produtos
