e590e955eb83890b39ce172fb382aec899c30b11
carbap <aca.baptista@gmail.com>
2016-05-11 19:48:50 +0100

    Update Transacao.cpp

--- Transacao.cpp
+++ Transacao.cpp
@@ -6,16 +6,21 @@ unsigned int Transacao::getIdCliente() const{
   return idCliente;
 }
 
-
-
 Transacao::Transacao(ifstream & in){ // le uma transacao na forma de  idcliente ; data ; lista produtos
   // A IMPLEMENTAR
 }
 
+Transacao::Transacao(unsigned int idCliente, Data data, vector<string> produtos)
+{
+	this->idCliente = idCliente;
+	this->data = data;
+	this->produtos = produtos;
+}
+
+
 void Transacao::save(ofstream & out) const{ // transacao guardada como na forma de  idcliente ; data ; lista produtos
   // A IMPLEMENTAR
 }
-
 
 ostream& operator<<(ostream& out, const Transacao & trans){
 
