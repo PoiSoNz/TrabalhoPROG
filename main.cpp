65a7b83f11a3215fabf489763ed0a91a7379847a
carbap <aca.baptista@gmail.com>
2016-05-11 19:37:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,59 +10,42 @@
 
 
 int main(){
-  string loja, fichClientes, fichProdutos, fichTransacoes;
-  ifstream fclientes, fprodutos, ftransacoes;
-  string line;
+  string loja, fichClients, fichProducts, fichTransactions;
+  ifstream fclients, fproducts, ftransactions;
   
+  //Nome da loja
+  cout << "Qual o nome da loja? ";
+  getline(cin, loja);
+  cout << endl;
+
   //Clientes
   cout << "Nome do ficheiro de clientes? ";
-  cin >> fichClientes;
-  checkFile(fclientes, fichClientes);
-  
+  cin >> fichClients;
+  checkFile(fclients, fichClients);
 
   //Produtos
   cout << "Nome do ficheiro de produtos? ";
-  cin >> fichProdutos;
-  checkFile(fprodutos, fichProdutos);
-  
-  while (getline(fproducts, line))
-  {
-	  string product = line;
-	  products.push_back(product);
-  }
-  //
-  cout << endl;
-  //
+  cin >> fichProducts;
+  checkFile(fproducts, fichProducts);
 
-  //Transacoes
+  //Ficheiro de transacoes
   cout << "Nome do ficheiro de transacoes? ";
-  cin >> fichTransacoes;
-  checkFile(ftransacoes, fichTransacoes);
-  
-  while (getline(ftransactions, line))
-  {
-	  int index = line.find_first_of(';');
-	  int index2 = line.find_last_of(';');
-	  string id = line.substr(0, index - 1);
-	  string date = line.substr(index + 2, (index2 - 1) - (index + 2));
-	  string list = line.substr(index2 + 2);
-	  Transactions_t t;
-	  t.id = stoi(id);
-	  t.date = date;
-	  t.productslist = list;
-	  transactions.push_back(t);
-  }
+  cin >> fichTransactions;
+  checkFile(ftransactions, fichTransactions);
 
   //
-  cout << endl;
-  //
+  VendeMaisMais obj = VendeMaisMais(loja, fichClients, fichProducts, fichTransactions);
+  obj.fillClientsVector(fclients);
+  obj.fillProductsVector(fproducts);
+  obj.fillTransactionsVector(ftransactions);
+
   // pede a informacoo sobre o nome da loja e os 3 ficheiros com
   // informacoo de clientes, produtos e transacoes
-  if(! infoInicial(loja, fichClients, fichProdutos, fichTransacoes))
+  if(!infoInicial(loja, fichClients, fichProducts, fichTransactions))
     return(-1);
 
   // cria uma loja
-  VendeMaisMais supermercado(loja, fichClients, fichProdutos, fichTransacoes);
+  VendeMaisMais supermercado(loja, fichClients, fichProducts, fichTransactions);
 
   cout << "Informacao da loja '" << loja << " do supermercado Vende++:" << endl;
   cout << supermercado << endl;  // mostra estatisticas da loja
