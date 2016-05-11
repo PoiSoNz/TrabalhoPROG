40e62673ba9861a8d2bd5d03ddb83727d4f9d60b
PoiSoNz <viizion-poisonz@hotmail.com>
2016-05-11 21:46:26 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -22,3 +22,7 @@ unsigned short int leUnsignedShortInt(unsigned short int min, unsigned short int
 int leInteiro(int min, int max);
 
 void checkFile(ifstream &stream, string nomeFicheiro);
+
+void trim(string &s);
+
+int encontraPosicao(string s, char *c, unsigned int num);// retorna a posição do numº caracter *c na string s
