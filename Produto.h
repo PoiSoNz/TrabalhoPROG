dac7fc1faedfa65c5716f81db2a60ce16b1e6cfe
carbap <aca.baptista@gmail.com>
2016-05-16 14:42:31 +0100

    Update Produto.h

--- Produto.h
+++ Produto.h
@@ -18,14 +18,11 @@ class Produto {
 
 
  public:
-  Produto(ifstream & in);
   Produto();
   Produto(string nome, float custo);
   string getNome() const;
   float getCusto() const;
   int indiceProduto(string nome); // uso do map para "traduzir" o nome em indice no vetor de produtos
-  friend ostream& operator<<(ostream& out, const Produto & prod); // ostra umproduto
+  friend ostream& operator<<(ostream& out, const Produto &prod); // ostra umproduto
   friend bool operator<(const Produto &prod1, const Produto &prod2); // compara 2 produtos (1 produto e mais pequeno que outro se o seu nome for "alfabeticamente inferior"
 };
-
-
