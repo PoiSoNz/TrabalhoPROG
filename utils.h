46240f0a443c379542d7d72a044445c229b65d7f
carbap <aca.baptista@gmail.com>
2016-05-16 14:51:49 +0100

    Update utils.h

--- utils.h
+++ utils.h
@@ -5,10 +5,10 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
-#include "Cliente.h"
-#include "Data.h"
-#include "Produto.h"
-#include "Vende++.h"
+//#include "Cliente.h"
+//#include "Data.h"
+//#include "Produto.h"
+//#include "Vende++.h"//ver se estes sao necessarios
 
 #include "defs.h"
 
@@ -23,6 +23,10 @@ int leInteiro(int min, int max);
 
 void checkFile(ifstream &stream, string nomeFicheiro);
 
-void trim(string &s);
+void extractProducts(vector<string> vetor, string listaprodutos);//extrai para o vetor de produtos(private a esta classe) cada produto duma lista de produtos
+
+void lowerCase(string &s);//converte cada caracter de uma string de C++ para minuscula
+
+void trim(string &s);//comenta isto sff
 
 int encontraPosicao(string s, char *c, unsigned int num);// retorna a posição do numº caracter *c na string s
