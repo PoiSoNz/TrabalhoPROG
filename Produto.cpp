40dd530ea62c60317ea9018451b750cac5e22c86
carbap <aca.baptista@gmail.com>
2016-05-16 14:41:22 +0100

    Update Produto.cpp

--- Produto.cpp
+++ Produto.cpp
@@ -1,12 +1,11 @@
 #include "Produto.h"
+#include "utils.h"
+#include <iomanip>
 
 
-Produto::Produto(ifstream & in){ // nome ; custo
-}
-
-Produto::Produto(){//Um produto criado por omissão não tem nome e tem custo zero.
-	nome="";
-	custo=0;
+Produto::Produto() {//Um produto criado por omissao nao tem nome(string vazia) e tem custo zero.
+	nome = "";
+	custo = 0;
 }
 
 Produto::Produto(string nome, float custo)
@@ -23,14 +22,27 @@ float Produto::getCusto() const {
   return custo;
 }
 
-ostream& operator<<(ostream& out, const Produto & prod){
-
-
-  // A IMPLEMENTAR
-
+ostream& operator<<(ostream& out, const Produto &prod){
+	out << setw(5) << left << prod.getNome() << setw(25) << fixed << setprecision(2) << prod.getCusto();
+	out.unsetf(ios_base::floatfield);
+	return out;
 }
 
 
 bool operator<(const Produto &prod1, const Produto &prod2){
-  // A IMPLEMENTAR
+	string nome1 = prod1.getNome();
+	string nome2 = prod2.getNome();
+	lowerCase(nome1);
+	lowerCase(nome2);
+	const char *nomeProduto1 = nome1.c_str();//converte o nome de cada um dos clientes para uma string de C
+	const char *nomeProduto2 = nome2.c_str();//para poder usar como argumentos da funcao strcmp
+
+	if ((strcmp(nomeProduto1, nomeProduto2)) < 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
