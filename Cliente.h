67dbc9c4001ce92e5bd79688228b566b11e98402
carbap <aca.baptista@gmail.com>
2016-05-22 15:49:19 +0100

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -5,6 +5,7 @@
 #include <string>
 
 #include "Data.h"
+//#include "utils.h"
 
 using namespace std;
 
@@ -17,12 +18,18 @@ class Cliente {
   float volCompras;
 
  public:
-  Cliente(ifstream & in);
-  Cliente();
-  Cliente(unsigned int id, string nome, Data cartaoCliente, float volCompras);//*****
+  Cliente::Cliente();
+  Cliente(unsigned int id, string nome, Data cartaoCliente, float volCompras);
   string getNome() const;
   unsigned int getId() const;
   float getVolCompras() const;
+  Data getcartaoCliente() const;
+  void setNome(string nome);
+  void setId(unsigned int id);
+  void setVolCompras(float volCompras);
+  void setcartaoCliente(Data cartaoCliente);
+  friend void lowerCase(string &s);//converte cada caracter de uma string de C++ para minuscula
+
   void save(ofstream & out) const;
   friend ostream& operator<<(ostream& out, const Cliente & cli);  // mostra informacao do cliente no ecra
   friend bool operator<(const Cliente &cli1, const Cliente &cli2); // compara 2 clientes (1 cliente e menor que outro se o seu nome for "alfabeticamente" inferior)
